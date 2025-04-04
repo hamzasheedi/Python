7ffa7159a9aebb881282a12a6affff85a8f229fc
Muhammad Hamza <attackerv21@gmail.com>
2025-04-04 22:22:53 +0500

    Create 02_agreement_bot.py

---
+++ 00_INTRO/02_agreement_bot/02_agreement_bot.py
@@ -0,0 +1,10 @@
+# Asking for user input
+fav_animal = input("\nEnter Your Favourite Animal: ")
+
+# Main function to print the message
+def main():
+    print(f"\nMy favorite animal is also {fav_animal}...!")
+
+# Calling the main function
+if __name__ == '__main__':
+    main()
