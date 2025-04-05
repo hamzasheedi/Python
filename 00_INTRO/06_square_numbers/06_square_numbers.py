b5638f1a0cfc90c61067a1fc3d4f3cec1cba943d
Muhammad Hamza <attackerv21@gmail.com>
2025-04-05 21:07:19 +0500

    Create 06_square_numbers.py

---
+++ 00_INTRO/06_square_numbers/06_square_numbers.py
@@ -0,0 +1,16 @@
+def main():
+    print("=== Square of a Number ===")
+
+    # Taking user input
+    number = float(input("\nEnter your number: "))
+
+    # Calculating the square
+    square = number * number
+
+    # Displaying the result
+    print(f"\nThe square of {number} is {square}")
+
+
+# Entry point of the program
+if __name__ == '__main__':
+    main
