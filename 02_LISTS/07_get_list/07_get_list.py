69fdac057700d1073fb9d393ec8f5e6ac069e6a7
Muhammad Hamza <attackerv21@gmail.com>
2025-04-11 01:54:02 +0500

    Update 07_get_list.py

--- 02_LISTS/07_get_list/07_get_list.py
+++ 02_LISTS/07_get_list/07_get_list.py
@@ -1,1 +1,17 @@
-07_get_list.py
+my_list: list[str] = []
+
+def make_list():
+    print("📥 Let's build a list! Press Enter without typing anything to finish.\n")
+
+    while True:
+        element = input("➕ Enter an element (or press Enter to finish): ")
+        if element == "":
+            print(f"\n✅ Final List: {my_list}")
+            return
+        my_list.append(element)
+
+def main():
+    make_list()
+
+if __name__ == '__main__':
+    main()
