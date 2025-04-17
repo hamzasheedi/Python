514d9337d91e780e79d25a70516f25c8315096b3
Muhammad Hamza <attackerv21@gmail.com>
2025-04-18 03:58:36 +0500

    Update 04_liftoff.py

--- 05_loops_control_flow/04_liftoff/04_liftoff.py
+++ 05_loops_control_flow/04_liftoff/04_liftoff.py
@@ -1,1 +1,15 @@
-04_liftoff.py
+def main():
+    # Using a for loop
+    for i in range(10, 0, -1):
+        print(i, end=" ")
+    print("Liftoff!")  # After the for loop finishes
+
+    # Using a while loop
+    n = 10
+    while n > 0:
+        print(n, end=" ")
+        n -= 1
+    print("Liftoff!")  # After while loop finishes
+
+if __name__ == '__main__':
+    main()
