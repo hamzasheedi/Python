70001c587bbd7bc76b854557c0918193979c8afe
Muhammad Hamza <attackerv21@gmail.com>
2025-04-18 04:02:19 +0500

    Update 05_double_it.py

--- 05_loops_control_flow/05_double_it/05_double_it.py
+++ 05_loops_control_flow/05_double_it/05_double_it.py
@@ -1,1 +1,8 @@
-05_double_it
+curr_value = int(input("Enter Your Number: "))
+
+while True:
+    curr_value *= 2
+    print(f"Current Value: {curr_value}")
+    if curr_value >= 100:
+        print("Done! Value has reached or exceeded 100.")
+        break
