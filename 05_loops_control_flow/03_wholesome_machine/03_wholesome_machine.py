6ed8472d47963afb9c5edd38d4ba4ff95db32664
Muhammad Hamza <attackerv21@gmail.com>
2025-04-17 06:27:38 +0500

    Update 03_wholesome_machine.py

--- 05_loops_control_flow/03_wholesome_machine/03_wholesome_machine.py
+++ 05_loops_control_flow/03_wholesome_machine/03_wholesome_machine.py
@@ -1,1 +1,16 @@
-03_wholesome_machine.py
+affirmation = "I am capable of doing anything I put my mind to."
+
+def main():
+    print("\n✨ Daily Affirmation ✨")
+    print(affirmation)
+    
+    while True:
+        user_input = input("\nType the affirmation exactly as shown above: ")
+        if user_input == affirmation:
+            print("✅ That's right! :)")
+            return
+        else:
+            print("❌ That was not the correct affirmation. Please try again!")
+
+if __name__ == '__main__':
+    main()
