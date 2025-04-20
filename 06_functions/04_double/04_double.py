5715ea9125476466089f5fe98faf11fdb1bcc270
Muhammad Hamza <attackerv21@gmail.com>
2025-04-21 03:18:43 +0500

    Update 04_double.py

--- 06_functions/04_double/04_double.py
+++ 06_functions/04_double/04_double.py
@@ -1,1 +1,14 @@
-04_double
+def double(num):
+    doubled = num * 2
+    print(f"Double that is {doubled}")
+
+def main():
+    try:
+        num = int(input("Enter Your Number To Double: "))
+    except:
+        print("Invalid input. Please enter a number.")
+        return
+    double(num)
+
+if __name__ == '__main__':
+    main()
