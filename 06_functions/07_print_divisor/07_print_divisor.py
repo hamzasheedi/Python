a870fabd392e1f2fe2c77ce1d6dac1f2ba04a79a
Muhammad Hamza <attackerv21@gmail.com>
2025-04-27 04:45:11 +0500

    Update 07_print_divisor.py

--- 06_functions/07_print_divisor/07_print_divisor.py
+++ 06_functions/07_print_divisor/07_print_divisor.py
@@ -1,1 +1,18 @@
+def print_divisors(num):
+    for i in range(1, num + 1):
+        divisor = num % i
+        if divisor == 0:
+            print(i)
+
+def main():
+    while True:
+        try:
+            num = int(input("Enter Your Number To Print Divisors: "))
+        except:
+            print("Input Only Integers")
+            break
+        print_divisors(num)
+
+if __name__ == '__main__':
+    main()
 
