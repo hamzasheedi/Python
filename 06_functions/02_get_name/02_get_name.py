7a0f487aa7857a88b69d4f30e36abbae76da2d08
Muhammad Hamza <attackerv21@gmail.com>
2025-04-27 04:15:49 +0500

    Update 02_get_name.py

--- 06_functions/02_get_name/02_get_name.py
+++ 06_functions/02_get_name/02_get_name.py
@@ -1,1 +1,12 @@
-02_get_name.py
+def get_name():
+    name = input("Enter Your Name: ")
+    return name
+
+def main():
+    name = get_name()
+    print("Howdy", name, "! 🤠")
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
