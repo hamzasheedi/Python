d64d29dd0d9e0edcb78ecd405828ec0568b4ad22
Muhammad Hamza <attackerv21@gmail.com>
2025-04-27 04:22:37 +0500

    Update 06_is_odd.py

--- 06_functions/06_is_odd/06_is_odd.py
+++ 06_functions/06_is_odd/06_is_odd.py
@@ -1,1 +1,18 @@
+def is_odd(value):
+    odd = value % 2
+    if odd != 0:
+        print(f"{value} is Odd!")
+
+def is_even(value):
+    even = value % 2
+    if even == 0:
+        print(f"{value} is Even!")
+
+def main():
+    for i in range(11):
+        is_even(i)
+        is_odd(i)
+
+if __name__ == '__main__':
+    main()
 
