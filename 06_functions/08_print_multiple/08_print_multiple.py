01b0f2bac6adf4f163c071fc17e70c08a6862dfb
Muhammad Hamza <attackerv21@gmail.com>
2025-04-28 11:15:26 +0500

    Update 08_print_multiple.py

--- 06_functions/08_print_multiple/08_print_multiple.py
+++ 06_functions/08_print_multiple/08_print_multiple.py
@@ -1,1 +1,19 @@
-08_print_multiple.py
+def print_multiple(message, repeats):
+    for _ in range(repeats):
+        print(message)
+
+def main():
+    while True:
+        try:
+            message = input("Enter Your Message: (Press Enter To Exit) ")
+            if message == "":
+                return
+            repeats = int(input("How Many Times You Want To Repeat: "))
+        except:
+            print("Please Try Again!!")
+            continue
+
+        print_multiple(message, repeats)
+
+if __name__ == '__main__':
+    main()
