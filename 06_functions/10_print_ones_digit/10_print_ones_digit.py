af93dcaf245968f42c1921b773198f6116c60511
Muhammad Hamza <attackerv21@gmail.com>
2025-04-29 20:31:59 +0500

    Update 10_print_ones_digit.py

--- 06_functions/10_print_ones_digit/10_print_ones_digit.py
+++ 06_functions/10_print_ones_digit/10_print_ones_digit.py
@@ -1,1 +1,15 @@
-10_print_ones_digit
+def print_ones_digit(numb):
+    last_digit = numb % 10
+    print(f"The ones digit is: {last_digit}")
+
+def main():
+    while True:
+        try:
+            numb = int(input("Enter Your Number (Press Enter To Exit): "))
+            print_ones_digit(numb)
+            break
+        except:
+            print("Invalid input. Please enter a valid integer.")
+
+if __name__ == '__main__':
+    main()
