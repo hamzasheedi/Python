c872d02f005eb8888a190a86ad699bdd2910d133
Muhammad Hamza <attackerv21@gmail.com>
2025-04-30 10:31:23 +0500

    Update 02_in_range.py

--- 07_information_flow/02_in_range/02_in_range.py
+++ 07_information_flow/02_in_range/02_in_range.py
@@ -1,1 +1,11 @@
-02_in_range
+def in_range(n, low, high):
+    if n > low and n < high:
+        return True
+    return False
+
+def main():
+    is_in_range = in_range(10, 1, 100)
+    print(is_in_range)
+
+if __name__ == '__main__':
+    main()
